47b4c805e29dd84f2fc48106d4633e4eb0c25baa
brainshower <matt@mattgenovese.net>
2013-01-02 22:23:34 -0600

    Checking in all files

---
+++ html_chamber_contact.php
@@ -0,0 +1,88 @@
+<?php
+if(isset($_POST['email'])) {
+     
+    // CHANGE THE TWO LINES BELOW
+    $email_to = "matt@mattgenovese.net";
+     
+    $email_subject = "Chamber Consulting Submission";
+     
+     
+    function died($error) {
+        // your error code can go here
+        echo "Sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please hit your browser's back button and fix these errors.<br /><br />";
+        die();
+    }
+     
+    // validation expected data exists
+    if(!isset($_POST['name']) ||
+        !isset($_POST['org']) ||
+        !isset($_POST['email'])) {
+        died('Sorry, but there appears to be a problem with the form you submitted.');      
+    }
+     
+    $first_name = $_POST['name']; // required
+    $title = $_POST['title']; 
+    $org = $_POST['org']; // required
+    $email_from = $_POST['email']; // required
+    $phone = $_POST['phone']; 
+    $comments = $_POST['message']; 
+    $spamtest = $_POST['phone_alt']; // if this is filled in, it's a bot (since it's not displayed via css).
+     
+    $error_message = "";
+
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The email address you entered does not appear to be valid.<br />';
+  }
+
+  $string_exp = "/^[A-Za-z .'-]+$/";
+  if(!preg_match($string_exp,$first_name)) {
+    $error_message .= 'The name you entered does not appear to be valid.<br />';
+  }
+
+  $string_exp = "/^[A-Za-z .'-]+$/";
+  if(!preg_match($string_exp,$org)) {
+    $error_message .= 'The organization you entered does not appear to be valid.<br />';
+  }
+
+  // This will check if the PHONE field is filled in.  If it is, it's probably a spam bot.
+  if(strlen($spamtest) > 0) {
+    $error_message .= '<br />';
+  }
+
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+    $email_message = "Form details below:\n\n";
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+     
+    $email_message .= "Name:  ".clean_string($first_name)."\n";
+    $email_message .= "Title: ".clean_string($title)."\n";
+    $email_message .= "Org:   ".clean_string($org)."\n";
+    $email_message .= "Phone: ".clean_string($phone)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Message:\n\n".clean_string($comments)."\n";
+     
+     
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers); 
+?>
+
+<!--Success!-->
+ 
+<?php
+}
+//include "index.html";
+header("Location: http://mattgenovese.net/index.html?m=1");
+die();
+?>
